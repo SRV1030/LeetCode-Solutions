4c4261bdd36fdcb2c8692a49dcdbdf2bc2984321
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-03-15 07:20:10 +0545

    Time: 24 ms (78.01%), Space: 11 MB (66.20%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        int c=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='(')c++;
+            else if(s[i]==')'){
+                if(!c)s[i]='#';
+                else c--;
+            }
+        }
+        c=0;
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==')')c++;
+            else if(s[i]=='('){
+                if(!c)s[i]='#';
+                else c--;
+            }
+        }
+        string ans;
+        for(int i=0;i<s.size();i++)
+            if(s[i]!='#')ans+=s[i];   
+         return ans;   
+    }
+};
