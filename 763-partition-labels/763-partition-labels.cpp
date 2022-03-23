54463ab63e1f86380a83614fa35b346290f36d9d
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-03-23 09:13:59 +0545

    Time: 4 ms (69.72%), Space: 6.5 MB (85.44%) - LeetHub

---
+++ 763-partition-labels/763-partition-labels.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+      vector<int> locur(26),ans;
+      for(int i=0;i<s.size();i++)locur[s[i]-'a']=i;
+      int b=0;
+      for(int i=0,j=0;i<s.size();i++){
+          j=max(j,locur[s[i]-'a']);
+          if(i==j){
+              ans.push_back(j-b+1);
+              b=i+1;
+          }
+      }
+      return ans;
+    }
+};
