d58d4301ccfc4cacc2a211069f757541ddeacd37
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-03-24 05:48:19 +0545

    Time: 63 ms (36.31%), Space: 8.5 MB (44.82%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:vector<vector<int>>dp;
+    int md=1000000007;
+    int util(int d,int f,int t){
+        if(d==0 && t==0) return 1;
+        if(d==0 || t<0) return 0;
+        if(dp[d][t]!=-1)return dp[d][t];
+        int ans=0;
+        for(int i=1;i<=f;i++)
+            ans=(ans%md+util(d-1,f,t-i)%md)%md;
+        
+        return dp[d][t]=ans;
+    }
+    int numRollsToTarget(int d, int f, int target) {
+        dp=vector<vector<int>>(d+1,vector<int>(target+1,-1));
+        return util(d,f,target);
+    }
+};
