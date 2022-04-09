6b5549820705cb8de6881e186bf07caa33a5edbf
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-04-09 09:03:38 +0545

    Time: 64 ms (97.41%), Space: 10.9 MB (93.95%) - LeetHub

--- 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
+++ 1745-palindrome-partitioning-iv/1745-palindrome-partitioning-iv.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-   bool isPal[2000][2000] = {};
-bool checkPartitioning(string s) {
-    for (int i = s.size() - 1; i >= 0; --i) {
-        for (int j = i; j < s.size(); ++j)
-            isPal[i][j] = s[i] == s[j] ? i + 1 >= j || isPal[i + 1][j - 1] : false;
-    }
-    for (auto i = 2; i < s.size(); ++i) {
-        if (isPal[i][s.size() - 1]) {
-            for (auto j = 1; j < i; ++j)
-                if (isPal[j][i - 1] && isPal[0][j - 1])
-                    return true;
+    bool isP[2000][2000] = {};
+    bool checkPartitioning(string s) {
+        for (int i = s.size() - 1; i >= 0; --i) {
+            for (int j = i; j < s.size(); ++j)
+                isP[i][j] = s[i] == s[j] ? i + 1 >= j || isP[i + 1][j - 1] : false;
         }
+        for (auto i = 2; i < s.size(); ++i) {
+            if (isP[i][s.size() - 1]) {
+                for (auto j = 1; j < i; ++j)
+                    if (isP[j][i - 1] && isP[0][j - 1])
+                        return true;
+            }
+        }
+        return false;
     }
-    return false;
-}
 };
