e53dd2bd3c53bb262f9932198e5a93310fb9baae
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-04-23 07:02:12 +0530

    Time: 7 ms (54.03%), Space: 7.8 MB (88.37%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int mx=0;
+        for(auto&i:accounts){
+            int s=0;
+            for(auto&j:i)s+=j;
+            mx=max(s,mx);
+        }
+        return mx;
+    }
+};
