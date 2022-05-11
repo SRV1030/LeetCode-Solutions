5017ec97e4b1605232b632baf27b9fe5ea9e2e06
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-05-11 16:48:05 +0530

    Time: 5 ms (23.36%), Space: 6.3 MB (83.25%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:vector<vector<int>> v;
+    void comb(vector<int> &a,int k,int sum,int ind){
+        if(sum==0 && k==0){
+            v.push_back(a);
+            return;
+        }
+        if(sum!=0 && k<=0) return;
+        for(int i=ind;i<10;i++){
+            a.push_back(i);
+            comb(a,k-1,sum-i,i+1);
+            a.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> a;
+        comb(a,k,n,1);
+        return v;
+    }
+};
