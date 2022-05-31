7bc47e27d8d1f0eb503f991b092f6c0aad85e322
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-05-31 07:31:11 +0530

    Time: 8 ms (64.20%), Space: 13.1 MB (53.92%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& p) {
+        int n=p.size();
+        int pr=0;
+        for(int i=0;i<n-1;i++){
+            if(p[i+1]>p[i]) pr+=p[i+1]-p[i];
+        }
+        return pr;
+    }
+};
