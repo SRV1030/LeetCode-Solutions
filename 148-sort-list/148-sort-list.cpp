9ff272727d98a59dd6e0f65bd83ac9362b2da2d3
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-06-13 13:44:00 +0530

    Time: 804 ms (5.03%), Space: 223.6 MB (5.03%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortedMerge(ListNode *a,ListNode *b){
+        if(!a) return b;
+        if(!b) return a;
+        ListNode* c=NULL;
+        if(a->val<=b->val){
+            c=new ListNode(a->val);
+            c->next=sortedMerge(a->next,b);
+        }
+        else{
+            c=new ListNode(b->val);
+            c->next=sortedMerge(a,b->next);
+        }
+        return c;
+    }
+    void divMid(ListNode *head,ListNode **a,ListNode **b){
+        if(!head || !head->next) return;
+        ListNode *s=head,*f=head->next;
+        while(f){
+            f=f->next;
+            if(f){
+                s=s->next;
+                f=f->next;
+            }
+        }
+        *a=head;
+        *b=s->next;
+        s->next=NULL;
+        return;        
+    }
+    ListNode* sortList(ListNode* head) {
+         if(!head || !head->next) return head;
+         ListNode *a=NULL,*b=NULL;
+        divMid(head,&a,&b);
+        a=sortList(a);
+        b=sortList(b);
+        head=sortedMerge(a,b);
+        return head;
+    }
+};
