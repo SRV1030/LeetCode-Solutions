74203db7c4b624f8ae0ddb3ffec29c5f34a2f609
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-06-16 13:55:42 +0530

    Time: 11 ms (10.81%), Space: 15.4 MB (28.03%) - LeetHub

---
+++ 78-subsets/78-subsets.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public: vector<vector<int>> res;
+    void util(vector<int> nums,int ind,vector<int> &temp){
+        if(ind==nums.size())res.push_back(temp);
+        else{
+            util(nums,ind+1,temp);
+            temp.push_back(nums[ind]);
+            util(nums,ind+1,temp);  
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        res.clear();
+        vector<int> temp={};
+        util(nums,0,temp);
+        return res;
+    }
+};
