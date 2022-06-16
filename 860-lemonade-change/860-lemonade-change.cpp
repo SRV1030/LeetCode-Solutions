3a59f254a36162e047deefe80cc9d43521527d20
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-06-16 12:55:05 +0530

    Time: 140 ms (57.33%), Space: 83.4 MB (48.49%) - LeetHub

--- 860-lemonade-change/860-lemonade-change.cpp
+++ 860-lemonade-change/860-lemonade-change.cpp
@@ -3,19 +3,13 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int fs=0,ts=0;
         for(auto&i:bills){
-            if(i==5)fs+=5;
-            else if(i==10){
-                ts+=i;
-                fs-=5;
-                if(fs<0) return false;
-            }else{
-                if(ts>0){
-                    ts-=10;
-                    fs-=5;
-                }
-                else fs-=15;
-                if(ts<0 || fs<0) return false;
+            if(i==5)fs++;
+            else if(i==10) fs--,ts++;
+            else{
+                if(ts)ts--,fs--;
+                else fs-=3;
             }
+            if(fs<0) return false;
         }
         return true;
     }
