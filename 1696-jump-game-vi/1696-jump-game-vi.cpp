a0043ecfe2335d696c2c7ed5c78e593e81926ee8
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-07-11 07:51:04 +0530

    Time: 355 ms (46.66%), Space: 93.9 MB (26.62%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        vector<int> dp(nums.size());
+        priority_queue<pair<int,int>> pq;
+        dp[0] = nums[0];
+        pq.push({nums[0],0});
+        for(int i=1;i<nums.size();i++){
+            dp[i]=nums[i];
+            while(pq.top().second+k<i)pq.pop();
+            dp[i]+=pq.top().first;
+            pq.push({dp[i],i});
+        }
+        return dp[nums.size()-1];
+    }
+};
