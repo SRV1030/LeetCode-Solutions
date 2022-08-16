c95f770222f0f909970b3fb9cb1fc20e9be339bf
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-08-16 09:22:20 +0530

    Time: 31 ms (83.25%), Space: 10.7 MB (43.91%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+int firstUniqChar(string s) {
+        int n = s.length();
+        vector<int> dp(26);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            dp[s[i] - 'a']++;
+            if (dp[s[i] - 'a'] == 1) st.push(i);
+            else if (!st.empty() && s[i] == s[st.top()]) {
+                while (!st.empty() && dp[s[st.top()] - 'a'] > 1) st.pop();
+            }
+        }
+        
+        return st.empty() ? -1 : st.top();
+    }
+};
