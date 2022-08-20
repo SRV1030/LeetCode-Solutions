114593f8f4f3c6d648073f4697b8223bf06ba0ea
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-08-20 07:28:59 +0530

    Time: 104 ms (47.09%), Space: 33.1 MB (49.20%) - LeetHub

--- 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.cpp
@@ -2,13 +2,9 @@ class Solution {
 public:
     int maxSumDivThree(vector<int>& nums) {
         vector<int>v(3);
-        for(auto&i:nums){
-            vector<int>t=v;
-            for(auto&j:t)
-                v[(i+j)%3]=max(v[(i+j)%3],i+j);                        
-        }
-        return v[0];
+        for(auto&i:nums)
+            for(int j:vector<int>(v))
+                v[(i+j)%3]=max(v[(i+j)%3],i+j);
+         return v[0];
     }
 };
-
-//Basically remove the least additive
