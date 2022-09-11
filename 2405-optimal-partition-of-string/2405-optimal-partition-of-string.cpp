3dd1818b00494ff4b79aa5449919bf9acd281a8d
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-09-11 15:00:40 +0530

    Time: 15 ms (100.00%), Space: 10.3 MB (20.00%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.cpp
@@ -1,14 +1,37 @@
+// class Solution {
+// public:
+//     int partitionString(string s) {
+//         set<char>st,ste;
+//         int c=1;
+//         for(auto&i:s){
+//             if(!st.insert(i).second){
+//                 st=ste;
+//                 st.insert(i);
+//                 ++c;
+//             }
+//         }
+//         return c;
+//     }
+// };
+
 class Solution {
 public:
+    int flag=0;
+    bool checkbit(int a){
+        return flag&(1<<a);
+    }
+    void setbit(int a){
+        flag|=(1<<a);
+    }
     int partitionString(string s) {
-        set<char>st,ste;
         int c=1;
         for(auto&i:s){
-            if(!st.insert(i).second){
-                st=ste;
-                st.insert(i);
+            int b=i-'a';
+            if(checkbit(b)){
+                flag=0;
                 ++c;
             }
+            setbit(b);
         }
         return c;
     }
