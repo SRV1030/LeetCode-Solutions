e5cfae16da960b14cdbf55f372d39bc8f2555447
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2022-11-17 17:30:21 +0530

    Time: 32 ms (50.21%), Space: 13.2 MB (61.82%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -1,9 +1,43 @@
+// class Solution {
+// public:
+//     int dp[100005]={};
+//     int countGoodStrings(int low, int high, int zero, int one,int i=0) {
+//         if(i>high) return 0;
+//         if(dp[i]==0) dp[i]=(1+(i>=low?1:0)+countGoodStrings(low,high,zero,one,i+zero)+countGoodStrings(low,high,zero,one,i+one))%1000000007;
+//         return dp[i]-1;
+//     }
+// };
+
+#define mod 1000000007
 class Solution {
 public:
-    int dp[100005]={};
-    int countGoodStrings(int low, int high, int zero, int one,int i=0) {
-        if(i>high) return 0;
-        if(dp[i]==0) dp[i]=(1+(i>=low?1:0)+countGoodStrings(low,high,zero,one,i+zero)+countGoodStrings(low,high,zero,one,i+one))%1000000007;
-        return dp[i]-1;
-    }
+int score(int target, vector<int>& dp, int one, int zero)
+{
+	// if target is 0 that means this target can be made so return 1
+	if (target == 0)
+		return 1;
+	// target < 0 cannot be made 
+	if (target < 0)
+		return 0;
+	// if already computed eturn the value
+	if (dp[target] != -1)
+		return dp[target];
+	long long sum;
+	// target - a is adding 'a' 0's to the string
+	// target - b is adding 'b' 1's to string 
+	sum =  score(target - one, dp, one, zero) + score(target - zero, dp,one, zero);
+	return dp[target] = (sum % (mod));
+}
+int countGoodStrings(int low, int high, int zero, int one)
+{
+	vector<int> dp(high + 1, -1);
+
+	int ans = 0;
+	// run a loop from low to high for every i get total number of ways to make that length
+	for (int i = low; i <= high; i++)
+	{
+		ans = ((ans % mod) + ( score(i, dp, one, zero) % mod )) % mod;
+	}
+	return ans;
+}
 };
