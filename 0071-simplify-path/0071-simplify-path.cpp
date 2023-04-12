0068070a7347cbe43a5d1de9a70ce4e3c862cca7
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2023-04-12 16:35:17 +0530

    Time: 7 ms (55.28%), Space: 9.6 MB (61.69%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        string res,temp;
+        vector<string>st;
+        stringstream str(path);
+        while(getline(str,temp,'/')){
+            if(temp=="" || temp==".") continue;
+            if(temp!="..")st.push_back(temp);
+            else if(!st.empty()) st.pop_back();
+        }
+        for(auto&s:st)res+="/"+s;
+        return res.empty()?"/":res;
+    }
+};
