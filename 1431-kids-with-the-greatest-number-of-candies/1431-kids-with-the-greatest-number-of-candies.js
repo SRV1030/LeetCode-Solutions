6bacc4a8f78d46b5196a4b6a1c8a3b74eb4c42fd
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2023-04-17 10:40:13 +0530

    Time: 71 ms (19.12%), Space: 42.5 MB (47.85%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.js
@@ -4,12 +4,6 @@
  * @return {boolean[]}
  */
 var kidsWithCandies = function(candies, extraCandies) {
-    var mx=candies.reduce((a, b) => Math.max(a, b));;
-    var ans=[];
-    candies.forEach((i)=>{
-            if(i+extraCandies>=mx)ans.push(true);
-            else ans.push(false);
-        }
-      );    
-    return ans;
+    const max = Math.max(...candies);
+    return candies.map(candy => candy + extraCandies >= max);
 };
