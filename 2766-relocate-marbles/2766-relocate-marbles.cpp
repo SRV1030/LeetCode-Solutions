05ae75d2a3639251bcc2af94d03bfd38fa275b4a
Sourabh Shah <65103790+SRV1030@users.noreply.github.com>
2023-07-19 09:40:10 +0530

    Time: 389 ms (71.49%), Space: 185.3 MB (55.68%) - LeetHub

--- 2766-relocate-marbles/2766-relocate-marbles.cpp
+++ 2766-relocate-marbles/2766-relocate-marbles.cpp
@@ -1,15 +1,28 @@
+// class Solution {
+// public:
+//     vector<int> relocateMarbles(vector<int>& nums, vector<int>& moveFrom, vector<int>& moveTo) {
+//         map<int,int>mp;
+//         for(auto&i:nums)++mp[i];
+//         for(int i=0;i<size(moveFrom);++i)
+//             if(moveTo[i]!=moveFrom[i] && mp.count(moveFrom[i])){
+//                 mp[moveTo[i]]+=mp[moveFrom[i]];
+//                 mp.erase(moveFrom[i]);
+//             }   
+//         vector<int>ans;
+//         for(auto&[k,v]:mp)ans.push_back(k);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> relocateMarbles(vector<int>& nums, vector<int>& moveFrom, vector<int>& moveTo) {
-        map<int,int>mp;
-        for(auto&i:nums)++mp[i];
-        for(int i=0;i<size(moveFrom);++i)
-            if(moveTo[i]!=moveFrom[i] && mp.count(moveFrom[i])){
-                mp[moveTo[i]]+=mp[moveFrom[i]];
-                mp.erase(moveFrom[i]);
-            }   
-        vector<int>ans;
-        for(auto&[k,v]:mp)ans.push_back(k);
-        return ans;
+        set<int> s(begin(nums), end(nums));
+        for (int i = 0; i < moveFrom.size(); ++i) {
+            s.erase(s.find(moveFrom[i]));
+            s.insert(moveTo[i]);
+        }
+        return vector<int>(begin(s), end(s));
     }
 };
+
